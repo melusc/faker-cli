47816fd80ff70625a1ba1acef9499d08a76ea57c
Luca <61409445+melusc@users.noreply.github.com>
2023-05-12 10:27:31 +0200

    Update image

--- src/commands/image.ts
+++ src/commands/image.ts
@@ -1,92 +1,139 @@
 import {faker} from '@faker-js/faker';
 
 import {BooleanFlag, Flag, createTemplate} from '../command-template.ts';
-import {transformInteger, transformNumber, identity} from '../util.ts';
+import {
+	identity,
+	integerBetween,
+	stringOf,
+	transformInteger,
+	transformNumber,
+} from '../util.ts';
 
 const module = faker.image;
 const template = createTemplate('image');
 
-const imageTemplate = [
-	new Flag({
-		flag: '--width <width>',
-		transform: transformInteger,
-	}),
-	new Flag({
-		flag: '--height <height>',
-		transform: transformInteger,
-	}),
-	new BooleanFlag({
-		flag: '--randomize',
-	}),
-] as const;
-
-template('abstract', imageTemplate, module.abstract);
-
-template('animals', imageTemplate, module.animals);
-
 template('avatar', [] as const, module.avatar);
 
-template('business', imageTemplate, module.business);
+template('avatarGitHub', [] as const, module.avatarGitHub);
 
-template('cats', imageTemplate, module.cats);
-
-template('city', imageTemplate, module.city);
+template('avatarLegacy', [] as const, module.avatarLegacy);
 
 template(
 	'dataUri',
 	[
-		new Flag({
-			flag: '--width <width>',
-			transform: transformNumber,
-		}),
-		new Flag({
-			flag: '--height <height>',
-			transform: transformNumber,
-		}),
-		new Flag({
-			flag: '--color <color>',
-			transform: identity,
-		}),
-	] as const,
+		{
+			width: new Flag({
+				flag: '--width <width>',
+				transform: transformNumber,
+			}),
+			height: new Flag({
+				flag: '--height <height>',
+				transform: transformNumber,
+			}),
+			color: new Flag({
+				flag: '--color <color>',
+				transform: identity,
+			}),
+		},
+	],
 	module.dataUri,
 );
 
-template('fashion', imageTemplate, module.fashion);
-
-template('food', imageTemplate, module.food);
-
-template('image', imageTemplate, module.image);
-
 template(
-	'imageUrl',
+	'url',
 	[
-		new Flag({
-			flag: '--width <width>',
-			transform: transformInteger,
-		}),
-		new Flag({
-			flag: '--height <height>',
-			transform: transformInteger,
-		}),
-		new Flag({
-			flag: '--category <category>',
-			transform: identity,
-		}),
-		new BooleanFlag({
-			flag: '--randomize',
-		}),
-	] as const,
-	module.imageUrl,
+		{
+			width: new Flag({
+				flag: '--width <width>',
+				transform: transformInteger,
+			}),
+			height: new Flag({
+				flag: '--height <height>',
+				transform: transformInteger,
+			}),
+		},
+	],
+	module.url,
 );
 
-template('nature', imageTemplate, module.nature);
+template(
+	'urlLoremFlickr',
+	[
+		{
+			category: new Flag({
+				flag: '--category <category>',
+				transform: identity,
+			}),
+			height: new Flag({
+				flag: '--height <height>',
+				transform: transformInteger,
+			}),
+			width: new Flag({
+				flag: '--width <width>',
+				transform: transformInteger,
+			}),
+		},
+	],
+	module.urlLoremFlickr,
+);
 
-template('nightlife', imageTemplate, module.nightlife);
+template(
+	'urlPicsumPhotos',
+	[
+		{
+			blur: new Flag({
+				flag: '--blur <level>',
+				transform: integerBetween(1, 10) as (
+					s: string,
+				) => 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10,
+			}),
+			grayscale: new BooleanFlag({
+				flag: '--grayscale',
+			}),
+			height: new Flag({
+				flag: '--height <height>',
+				transform: transformInteger,
+			}),
+			width: new Flag({
+				flag: '--width <width>',
+				transform: transformInteger,
+			}),
+		},
+	],
+	module.urlPicsumPhotos,
+);
 
-template('people', imageTemplate, module.people);
-
-template('sports', imageTemplate, module.sports);
-
-template('technics', imageTemplate, module.technics);
-
-template('transport', imageTemplate, module.transport);
+template(
+	'urlPlaceholder',
+	[
+		{
+			backgroundColor: new Flag({
+				flag: '--background-color <color>',
+				transform: identity,
+			}),
+			format: new Flag({
+				flag: '--format <image format>',
+				transform: stringOf(
+					new Set(['gif', 'jpeg', 'jpg', 'png', 'webp'] as const),
+				),
+			}),
+			height: new Flag({
+				flag: '--height <height>',
+				transform: transformInteger,
+			}),
+			text: new Flag({
+				flag: '--text <text>',
+				transform: identity,
+			}),
+			textColor: new Flag({
+				flag: '--text-color <color>',
+				transform: identity,
+			}),
+			width: new Flag({
+				flag: '--width <width>',
+				transform: transformInteger,
+			}),
+		},
+	],
+	module.urlPlaceholder,
+);
