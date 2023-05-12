8b9cfece1da3df71cf5e175d06e574a0d8199ffb
Luca <61409445+melusc@users.noreply.github.com>
2023-05-12 08:51:47 +0200

    Add airline

---
+++ src/commands/airline.ts
@@ -0,0 +1,67 @@
+import {faker} from '@faker-js/faker';
+
+import {BooleanFlag, Flag, createTemplate} from '../command-template.ts';
+import {stringOf, transformInteger} from '../util.ts';
+
+const module = faker.airline;
+const template = createTemplate('airline');
+
+template('aircraftType', [] as const, module.aircraftType);
+
+template('airline', [] as const, module.airline);
+
+template('airplane', [] as const, module.airplane);
+
+template('airport', [] as const, module.airport);
+
+template(
+	'flightNumber',
+	[
+		{
+			length: {
+				min: new Flag({
+					flag: '--min <length>',
+					transform: transformInteger,
+				}),
+				max: new Flag({
+					flag: '--max <length>',
+					transform: transformInteger,
+				}),
+			},
+			addLeadingZeros: new BooleanFlag({
+				flag: '--add-leading-zeros',
+			}),
+		},
+	] as const,
+	module.flightNumber,
+);
+
+template(
+	'recordLocator',
+	[
+		{
+			allowNumerics: new BooleanFlag({
+				flag: '--allow-numberics',
+			}),
+			allowVisuallySimilarCharacters: new BooleanFlag({
+				flag: '--allow-visually-similar-characters',
+			}),
+		},
+	],
+	module.recordLocator,
+);
+
+template(
+	'seat',
+	[
+		{
+			aircraftType: new Flag({
+				flag: '--aircraft-type <type>',
+				transform: stringOf(
+					new Set(['narrowbody', 'regional', 'widebody'] as const),
+				),
+			}),
+		},
+	],
+	module.seat,
+);
